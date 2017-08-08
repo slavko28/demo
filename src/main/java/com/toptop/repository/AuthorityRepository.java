0de2c31680fe1e83868bab33b802c30f291ece3d
Yaroslav Soltys <ce.prosto@gmail.com>
2017-08-08 12:56:59 +0300

    added queries for security

--- src/main/java/com/toptop/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toptop.repository;
-
-import com.toptop.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Integer>{
-}
