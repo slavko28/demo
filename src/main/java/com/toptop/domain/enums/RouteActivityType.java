b286ba8ccabae073938a02b714560a0399e9cec8
Yaroslav Soltys <ce.prosto@gmail.com>
2017-08-09 11:11:29 +0300

    added Route activity type (RECEIVING and DELIVERY)

---
+++ src/main/java/com/toptop/domain/enums/RouteActivityType.java
@@ -0,0 +1,14 @@
+package com.toptop.domain.enums;
+
+public enum RouteActivityType {
+
+    /**
+     * Acceptance of cargo for transportation.
+     */
+    CARGO_RECEIVING,
+
+    /**
+     * Unloading of the delivered cargo.
+     */
+    CARGO_DELIVERY
+}
