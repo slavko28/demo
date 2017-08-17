06750a1bbff135ed709dc22aafeb388e3b310ee4
Yaroslav Soltys <ce.prosto@gmail.com>
2017-08-17 22:39:37 +0300

    added abstract service with CRUD operations

---
+++ src/main/java/com/toptop/service/AbstractService.java
@@ -0,0 +1,40 @@
+package com.toptop.service;
+
+import com.toptop.domain.BaseObject;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface AbstractService<T extends BaseObject, ID extends Serializable, DTO extends Object> {
+
+    /**
+     * Save new entity.
+     *
+     * @param entity the entity to save
+     * @return the saved entity
+     */
+    DTO save(T entity);
+
+    /**
+     * Find entity by Id.
+     *
+     * @param id the entity id
+     * @return the entity
+     */
+    DTO findOne(ID id);
+
+    /**
+     * Find all entities.
+     *
+     * @return the list of entities
+     */
+    List<DTO> findAll();
+
+    /**
+     * Deletes a given entity.
+     *
+     * @param id the entity id to delete
+     * @throws IllegalArgumentException in case the given entity is {@literal null}.
+     */
+    void delete(ID id);
+}
