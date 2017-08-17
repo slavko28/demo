fc3fbe1919686a34d6aba3c6ad02bec077168ff0
Yaroslav Soltys <ce.prosto@gmail.com>
2017-08-17 22:44:53 +0300

    added Transaction Service with basic operations

---
+++ src/main/java/com/toptop/service/impl/TransactionService.java
@@ -0,0 +1,46 @@
+package com.toptop.service.impl;
+
+import com.toptop.domain.BaseObject;
+import com.toptop.service.AbstractService;
+import com.toptop.service.mapper.BaseMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class TransactionService<T extends BaseObject, ID extends Serializable,
+        M extends BaseMapper<T, DTO>, DTO extends Object> implements AbstractService<T, ID, DTO> {
+
+    @Override
+    @Transactional
+    public DTO save(T entity) {
+        return getMapper().mapToDTO(getRepository().save(entity));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public DTO findOne(ID id) {
+        return getMapper().mapToDTO(getRepository().findOne(id));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<DTO> findAll() {
+        return getMapper().mapToDTOs(getRepository().findAll());
+    }
+
+    @Override
+    public void delete(ID id) {
+        T entity = getRepository().findOne(id);
+        if (entity == null) {
+            throw new IllegalArgumentException("Entity does not exists, ID: " + id);
+        }
+        entity.setEnabled(false);
+        getRepository().save(entity);
+    }
+
+    protected abstract JpaRepository<T, ID> getRepository();
+
+    protected abstract M getMapper();
+}
