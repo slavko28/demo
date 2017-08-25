6f6c0144a25392ebc00c89ebb2b0a94f4732fb3a
Yaroslav Soltys <ce.prosto@gmail.com>
2017-08-25 14:41:25 +0300

    update address repository

--- src/main/java/com/toptop/repository/AddressRepository.java
+++ src/main/java/com/toptop/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package com.toptop.repository;
 
 import com.toptop.domain.Address;
+import com.toptop.domain.enums.AddressType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +13,10 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
     @Query("select address from Company as company join company.address as address " +
             "where company.id = :id and address.enabled = true")
     List<Address> findAllByCompanyId(@Param("id") Long id);
+
+    @Query("select address from Company as company join company.address as address " +
+            "where company.id = :id and address.addressType = :addressType and address.enabled = true")
+    List<Address> findAllByCompanyIdAndType(@Param("id") Long id, @Param("addressType") AddressType addressType);
+
+    List<Address> findAllByAddressType(AddressType type);
 }
