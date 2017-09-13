2335c36771b67a0ab0cf40aefa1113995cbe0bcc
Yaroslav Soltys <ce.prosto@gmail.com>
2017-09-13 21:06:02 +0300

    updated repository

--- src/main/java/com/toptop/repository/CompanyOrderRepository.java
+++ src/main/java/com/toptop/repository/CompanyOrderRepository.java
@@ -3,6 +3,8 @@ package com.toptop.repository;
 import com.toptop.domain.CompanyOrder;
 import com.toptop.domain.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +15,9 @@ public interface CompanyOrderRepository extends JpaRepository<CompanyOrder, Long
     List<CompanyOrder> findAllByCompanyId(Long id);
 
     List<CompanyOrder> findAllByStatus(OrderStatus orderStatus);
+
+    @Query(value = "SELECT * FROM company_order AS c_o LEFT JOIN company_order_order_detail AS c_o_o_d" +
+            " ON c_o.id = c_o_o_d.company_order_id LEFT JOIN order_detail AS o_d" +
+            " ON c_o_o_d.order_detail_id = o_d.id WHERE o_d.manager_id = :userId", nativeQuery = true) // TODO: change to HQL
+    List<CompanyOrder> findAllByUserId(@Param("userId") Long id);
 }
--- src/main/java/com/toptop/repository/UserRepository.java
+++ src/main/java/com/toptop/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.toptop.repository;
 import com.toptop.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Created by slavkosoltys on 30.07.17.
  */
 public interface UserRepository extends JpaRepository <User, Long> {
-    User findByEmail(String email);
+    User findOneByEmail(String email);
 
 }
