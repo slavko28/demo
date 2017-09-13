a7108b109b149b3736e0004f018061b1b39e768f
Yaroslav Soltys <ce.prosto@gmail.com>
2017-09-13 21:10:50 +0300

    updated Application.java

--- src/main/java/com/toptop/Application.java
+++ src/main/java/com/toptop/Application.java
@@ -1,13 +1,16 @@
 package com.toptop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 
 /**
  * Created by slavkosoltys on 30.07.17.
  */
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {FreeMarkerAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
