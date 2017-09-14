457ddffb95fcd264614163543bbb278fc3ca0769
Yaroslav Soltys <ce.prosto@gmail.com>
2017-09-14 20:56:54 +0300

    updated repository

--- src/main/java/com/toptop/repository/CompanyOrderRepository.java
+++ src/main/java/com/toptop/repository/CompanyOrderRepository.java
@@ -16,8 +16,7 @@ public interface CompanyOrderRepository extends JpaRepository<CompanyOrder, Long
 
     List<CompanyOrder> findAllByStatus(OrderStatus orderStatus);
 
-    @Query(value = "SELECT * FROM company_order AS c_o LEFT JOIN company_order_order_detail AS c_o_o_d" +
-            " ON c_o.id = c_o_o_d.company_order_id LEFT JOIN order_detail AS o_d" +
-            " ON c_o_o_d.order_detail_id = o_d.id WHERE o_d.manager_id = :userId", nativeQuery = true) // TODO: change to HQL
+    @Query("select orders from CompanyOrder orders left join orders.orderDetail details where orders.enabled = true " +
+            "and details.manager.id = :userId")
     List<CompanyOrder> findAllByUserId(@Param("userId") Long id);
 }
--- src/main/java/com/toptop/repository/OrderDetailsRepository.java
+++ src/main/java/com/toptop/repository/OrderDetailsRepository.java
@@ -2,6 +2,8 @@ package com.toptop.repository;
 
 import com.toptop.domain.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
