67624c7eaaa9f232f95c561ce376b3c52632ccec
Yaroslav Soltys <ce.prosto@gmail.com>
2017-10-24 22:04:45 +0300

    disabled csrf

--- src/main/java/com/toptop/config/SpringSecurityConfig.java
+++ src/main/java/com/toptop/config/SpringSecurityConfig.java
@@ -31,7 +31,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/", "/login").permitAll()
                 .antMatchers("/user/create").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
