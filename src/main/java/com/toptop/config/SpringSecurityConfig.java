74627b826b91a8ef23d96526695e0f3f00537b52
Yaroslav Soltys <ce.prosto@gmail.com>
2017-10-26 14:42:36 +0300

    updated security

--- src/main/java/com/toptop/config/SpringSecurityConfig.java
+++ src/main/java/com/toptop/config/SpringSecurityConfig.java
@@ -32,7 +32,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/", "/login", "/reset/**").permitAll()
                 .antMatchers("/user/create").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/manager/**").hasAuthority("MANAGER")
