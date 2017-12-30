4213903b2e4d01a005df5c523157d17b36595e38
Yaroslav Soltys <ce.prosto@gmail.com>
2017-12-30 20:34:28 +0200

    changes for cloud deploy

--- src/main/java/com/toptop/Application.java
+++ src/main/java/com/toptop/Application.java
@@ -1,18 +1,23 @@
 package com.toptop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by slavkosoltys on 30.07.17.
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
